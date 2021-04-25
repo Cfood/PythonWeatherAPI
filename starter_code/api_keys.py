28ccdad9aff6b6ad30ba88375d95f23cbe5fef94
CYM <cyloumiller@gmail.com>
2021-04-25 17:37:22 -0500

    Commit

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "7cc1e6920f22e57faf6d2114d77a5456"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAoVXj_TBvQCZLtDTi2G34bIO_P7fSp9dE"
